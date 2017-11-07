f239aec3cc7fd3354f272d7e5fc8a52b58404fc0
Alyssom Falkenberg Martins <alyssommartins@gmail.com>
2017-11-07 19:59:08 -0200

    Troca de páginas
    Esta funcionando as trocas de páginas clicando sobre uma barbearia da lista esta indo para a página de Detalhes,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { BarbeariaDetailPage} from '../pages/barbeariaDetail/barbeariaDetail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -29,7 +30,8 @@ var config = {
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    BarbeariaDetailPage
   ],
   imports: [
     BrowserModule,
@@ -45,7 +47,8 @@ var config = {
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    BarbeariaDetailPage
   ],
   providers: [
     StatusBar,
