fc122cbc910f6bbc89aac08cb94868c60ab8018c
Alyssom Falkenberg Martins <alyssommartins@gmail.com>
2017-11-15 16:54:09 -0200

    Adds
    Foi adicionado junto ao Designer Bernardo um fundo na tela Principal da lista e na tela de Detalhes da barberia, também foi inserido uma foto a cada um das barbearias.

--- src/pages/barbeariaDetail/barbeariaDetail.ts
+++ src/pages/barbeariaDetail/barbeariaDetail.ts
@@ -10,10 +10,13 @@ export class BarbeariaDetailPage {
  
 nome ;
 logradouro; 
+foto;
+servicos;
   constructor(public navCtrl: NavController, public NavParams : NavParams) {
      this.nome = this.NavParams.data.obj.nome;
- 
      this.logradouro = this.NavParams.data.obj.logradouro;
+     this.foto = this.NavParams.data.obj.foto;
+     this.servicos = this.NavParams.data.obj.servicos;
 
   }
 
